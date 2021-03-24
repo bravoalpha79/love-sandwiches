54aef5b6aa4439fdb133cdd0aa94587368269d41
Igor Basuga <igor.basuga@gmail.com>
2021-03-24 14:23:47 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 
 SCOPE = [
@@ -24,7 +23,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
 
@@ -129,6 +128,7 @@ def calculate_stock_data(data):
     """
     Calculate the average stock for each item type, adding 10%
     """
+    print("Calculating stock data...\n")
     new_stock_data = []
 
     for column in data:
